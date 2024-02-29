a2b96f29bbcc15b777ba0b58191c84b3ae8cef86
Kenny <27463495+Frontesque@users.noreply.github.com>
2024-02-28 19:57:11 -0500

    install daisyui

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn">Hello daisyui</button>
   </div>
 </template>
 
