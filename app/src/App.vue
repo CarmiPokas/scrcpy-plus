01b8122ed6bad7703849a5e5ea1f550c0d5ca28a
Kenny <27463495+Frontesque@users.noreply.github.com>
2024-02-28 20:01:40 -0500

    fix legability of defaults

--- app/src/App.vue
+++ app/src/App.vue
@@ -12,7 +12,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: lightgray;
 }
 
 nav {
@@ -21,7 +21,7 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: lightgray;
 }
 
 nav a.router-link-exact-active {
